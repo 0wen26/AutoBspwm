5dd2f9608040bd7c10966c31cd1ee3b8378231c6
0wen26 <47756558+0wen26@users.noreply.github.com>
2026-01-13 16:16:07 +0100

    Refactor install.sh for improved output and structure

--- install.sh
+++ install.sh
@@ -28,27 +28,30 @@ real_home="/home/$real_user"
 
 # --- 1. DEPENDENCIAS ---
 function install_dependencies() {
-  echo -e "\n${yellowColour}[*] Actualizando sistema e instalando dependencias... ${endColour}\n"
-  apt update
+  echo -e "\n${yellowColour}[*] Actualizando sistema e instalando dependencias (esto tardará un poco)... ${endColour}"
+  
+  # Redirigimos a /dev/null para que no moleste
+  apt update > /dev/null 2>&1
 
-  # Añadimos zsh y dependencias de red
+  echo -e "   [i] Instalando librerías gráficas y de compilación..."
   apt install -y build-essential git vim xcb cmake pkg-config \
   libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev \
   libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev \
   libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev \
   libxcb-xkb-dev libcairo2-dev libx11-xcb-dev libxcb-composite0-dev \
   libxcb-image0-dev libxcb-cursor-dev xcb-proto python3-xcbgen \
-  rofi feh fzf curl wget unzip zsh
+  rofi feh fzf curl wget unzip zsh \
+  dunst libnotify-bin flameshot scrot lxappearance papirus-icon-theme > /dev/null 2>&1
 }
 
 # --- 2. CONFIGURACIÓN (DOTFILES) ---
 function install_dotfiles() {
-  echo -e "\n${turquoiseColour}[*] Enlazando configuraciones desde carpeta 'conf'...${endColour}\n"
+  echo -e "\n${turquoiseColour}[*] Enlazando configuraciones desde carpeta 'config'...${endColour}"
 
   repo_conf_dir="$(dirname "$(readlink -f "$0")")/config"
 
   if [ ! -d "$repo_conf_dir" ]; then
-      echo -e "${redColour}[!] Error: No encuentro la carpeta 'conf'.${endColour}"
+      echo -e "${redColour}[!] Error: No encuentro la carpeta 'config'.${endColour}"
       exit 1
   fi
 
@@ -64,86 +67,68 @@ function install_dotfiles() {
           fi
           ln -sf "$folder" "$real_home/.config/"
           
-          # Root (Opcional)
+          # PERMISOS AUTOMÁTICOS
+          if [ "$app_name" == "bspwm" ]; then
+              chmod +x "$real_home/.config/bspwm/bspwmrc"
+          fi
+          if [ -d "$real_home/.config/$app_name/bin" ]; then
+              chmod +x "$real_home/.config/$app_name/bin/"*
+          fi
+          
+          # Root
           mkdir -p /root/.config
           rm -rf "/root/.config/$app_name"
           cp -r "$folder" "/root/.config/"
       fi
   done
   
-  # --- FIX POLYBAR: Permisos y archivos faltantes ---
-  echo -e "   [i] Aplicando correcciones a scripts de Polybar..."
-  
-  # 1. Dar permisos de ejecución a scripts en ~/.config/bin (si existe)
-  if [ -d "$real_home/.config/bin" ]; then
-      chmod +x "$real_home/.config/bin/"*
-  fi
-
-  # 2. Fix error visual /bin/cat (suele ser por falta de target.txt)
-  # Creamos un archivo vacío para que no de error al leerlo
+  # --- FIX POLYBAR ---
+  echo -e "   [i] Aplicando parches automáticos a Polybar..."
+  mkdir -p "$real_home/.config/bin"
   touch "$real_home/.config/bin/target" 2>/dev/null
 
-  # 3. Detectar Interfaz de Red (eth0 vs ens33) y parchear Polybar
-  # Esto arregla el módulo de red que no aparece o da error
   default_iface=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
   if [ -n "$default_iface" ]; then
-      echo -e "   [i] Interfaz detectada: $default_iface. Parcheando Polybar..."
-      # Buscamos archivos .ini en polybar y reemplazamos la interfaz genérica
       find "$real_home/.config/polybar" -name "*.ini" -exec sed -i "s/interface = .*/interface = $default_iface/g" {} +
   fi
 
   chown -R "$real_user:$real_user" "$real_home/.config"
-  echo -e "${greenColour}[+] Dotfiles enlazados y corregidos.${endColour}"
+  echo -e "${greenColour}[+] Dotfiles listos.${endColour}"
 }
 
 # --- 3. ZSH + OMZ + P10K ---
 function install_zsh_omz() {
-  echo -e "\n${blueColour}[*] Instalando ZSH, Oh My Zsh y Powerlevel10k...${endColour}\n"
+  echo -e "\n${blueColour}[*] Instalando ZSH, Oh My Zsh y Powerlevel10k...${endColour}"
 
-  # 1. Instalar Oh My Zsh (Sin interacción)
   if [ ! -d "$real_home/.oh-my-zsh" ]; then
-      echo -e "   [i] Instalando Oh My Zsh..."
-      # Usamos 'su' para instalarlo como el usuario normal, no como root
-      su - "$real_user" -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
+      echo -e "   [i] Descargando Oh My Zsh..."
+      # Redirigimos la salida del script de instalación
+      su - "$real_user" -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended' > /dev/null 2>&1
   fi
 
-  # 2. Instalar Powerlevel10k
   p10k_dir="$real_home/.oh-my-zsh/custom/themes/powerlevel10k"
   if [ ! -d "$p10k_dir" ]; then
-      echo -e "   [i] Clonando Powerlevel10k..."
-      git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$p10k_dir"
+      echo -e "   [i] Instalando tema Powerlevel10k..."
+      git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$p10k_dir" > /dev/null 2>&1
   fi
 
-  # 3. Plugins comunes (Autosuggestions & Syntax Highlighting)
   zsh_custom="$real_home/.oh-my-zsh/custom/plugins"
-  git clone https://github.com/zsh-users/zsh-autosuggestions.git "$zsh_custom/zsh-autosuggestions" 2>/dev/null
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$zsh_custom/zsh-syntax-highlighting" 2>/dev/null
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git "$zsh_custom/zsh-autosuggestions" > /dev/null 2>&1
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$zsh_custom/zsh-syntax-highlighting" > /dev/null 2>&1
 
-  # 4. Enlazar .zshrc y .p10k.zsh si existen en el repo
-  repo_conf_dir="$(dirname "$(readlink -f "$0")")/conf"
-  
-  # Buscamos si tienes los archivos de zsh en tu carpeta conf
-  # A veces la gente los pone en conf/zsh/ o sueltos en conf/
+  repo_conf_dir="$(dirname "$(readlink -f "$0")")/config"
   if [ -f "$repo_conf_dir/zsh/.zshrc" ]; then
       ln -sf "$repo_conf_dir/zsh/.zshrc" "$real_home/.zshrc"
       ln -sf "$repo_conf_dir/zsh/.p10k.zsh" "$real_home/.p10k.zsh"
-  elif [ -f "$repo_conf_dir/.zshrc" ]; then
-      ln -sf "$repo_conf_dir/.zshrc" "$real_home/.zshrc"
-      ln -sf "$repo_conf_dir/.p10k.zsh" "$real_home/.p10k.zsh"
   else
-      echo -e "${yellowColour}[!] No encontré .zshrc en tu repo. Usando el por defecto.${endColour}"
-      # Activamos el tema powerlevel10k en el archivo por defecto
       sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' "$real_home/.zshrc"
   fi
 
-  # 5. Cambiar shell por defecto a zsh
-  chsh -s $(which zsh) "$real_user"
-  chsh -s $(which zsh) root
+  chsh -s $(which zsh) "$real_user" > /dev/null 2>&1
+  chsh -s $(which zsh) root > /dev/null 2>&1
+  chown -R "$real_user:$real_user" "$real_home/.oh-my-zsh" "$real_home/.zshrc" "$real_home/.p10k.zsh" "$p10k_dir"
 
-  # Permisos
-  chown -R "$real_user:$real_user" "$real_home/.oh-my-zsh" "$real_home/.zshrc" "$p10k_dir"
-
-  echo -e "${greenColour}[+] Entorno ZSH configurado.${endColour}"
+  echo -e "${greenColour}[+] ZSH configurado.${endColour}"
 }
 
 # --- 4. FUENTES ---
@@ -157,9 +142,9 @@ function install_fonts() {
     fc-cache -v > /dev/null 2>&1
     echo -e "${greenColour}[+] Fuentes locales instaladas.${endColour}"
   else
-    echo -e "${yellowColour}[!] Carpeta 'fonts' no encontrada. Descargando Hack Nerd Font...${endColour}"
-    wget "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.zip" -O Hack.zip
-    unzip -o Hack.zip -d "$fonts_dest"
+    echo -e "${yellowColour}[!] Descargando Hack Nerd Font de internet...${endColour}"
+    wget -q "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Hack.zip" -O Hack.zip
+    unzip -o Hack.zip -d "$fonts_dest" > /dev/null 2>&1
     rm Hack.zip
     fc-cache -v > /dev/null 2>&1
   fi
@@ -167,70 +152,76 @@ function install_fonts() {
 
 # --- 5. BSPWM & SXHKD ---
 function install_bspwm_sxhkd() {
-  echo -e "\n${blueColour}[*] Instalando BSPWM y SXHKD...${endColour}\n"
+  echo -e "\n${blueColour}[*] Compilando e Instalando BSPWM y SXHKD...${endColour}"
   cd /usr/local/src
   rm -rf bspwm sxhkd
 
-  git clone https://github.com/baskerville/bspwm.git
-  git clone https://github.com/baskerville/sxhkd.git
+  # Silenciamos git
+  git clone https://github.com/baskerville/bspwm.git > /dev/null 2>&1
+  git clone https://github.com/baskerville/sxhkd.git > /dev/null 2>&1
 
-  cd bspwm && make && make install
+  cd bspwm
+  make > /dev/null 2>&1
+  make install > /dev/null 2>&1
   cp contrib/freedesktop/bspwm.desktop /usr/share/xsessions/
 
-  cd ../sxhkd && make && make install
+  cd ../sxhkd
+  make > /dev/null 2>&1
+  make install > /dev/null 2>&1
 
   cd ~
-  echo -e "${greenColour}[+] BSPWM y SXHKD instalados.${endColour}"
+  echo -e "${greenColour}[+] BSPWM y SXHKD listos.${endColour}"
 }
 
 # --- 6. POLYBAR ---
 function install_polybar() {
-  echo -e "\n${purpleColour}[*] Instalando Polybar...${endColour}\n"
-  apt install -y libuv1-dev libxml2-dev 2>/dev/null
+  echo -e "\n${purpleColour}[*] Compilando Polybar (esto tarda unos segundos)...${endColour}"
+  apt install -y libuv1-dev libxml2-dev > /dev/null 2>&1
 
   cd /usr/local/src
   rm -rf polybar
-  git clone --recursive https://github.com/polybar/polybar.git
+  git clone --recursive https://github.com/polybar/polybar.git > /dev/null 2>&1
 
   cd polybar
   mkdir build && cd build
-  cmake .. -DBUILD_DOC=OFF -DENABLE_CURL=OFF
-  make -j$(nproc)
-  make install
+  cmake .. -DBUILD_DOC=OFF -DENABLE_CURL=OFF > /dev/null 2>&1
+  make -j$(nproc) > /dev/null 2>&1
+  make install > /dev/null 2>&1
 
-  echo -e "${greenColour}[+] Polybar instalado.${endColour}"
+  echo -e "${greenColour}[+] Polybar instalada.${endColour}"
 }
 
 # --- 7. PICOM ---
 function install_picom() {
-  echo -e "\n${purpleColour}[+] Instalando Picom...${endColour}"
+  echo -e "\n${purpleColour}[+] Compilando Picom...${endColour}"
+  # Deps extra silenciosas
   apt install -y libconfig-dev libdbus-1-dev libegl-dev libev-dev libepoxy-dev \
     libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev \
     libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev \
     libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev \
-    libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev
+    libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev > /dev/null 2>&1
 
   cd /usr/local/src
   rm -rf picom
-  git clone https://github.com/yshui/picom.git
+  git clone https://github.com/yshui/picom.git > /dev/null 2>&1
   cd picom
-  meson setup --buildtype=release build 
-  ninja -C build
-  ninja -C build install
+  meson setup --buildtype=release build > /dev/null 2>&1
+  ninja -C build > /dev/null 2>&1
+  ninja -C build install > /dev/null 2>&1
 
   echo -e "${greenColour}[+] Picom instalado.${endColour}"
 }
 
 # --- 8. KITTY ---
 function install_kitty() {
-  echo -e "\n${blueColour}[*] Instalando Kitty Terminal...${endColour}\n"
+  echo -e "\n${blueColour}[*] Instalando Kitty Terminal...${endColour}"
   cd /opt
-  # Enlace directo universal
-  wget "https://github.com/kovidgoyal/kitty/releases/download/v0.45.0/kitty-0.45.0-x86_64.txz" -O kitty.txz
+  # wget -q es modo silencioso
+  wget -q "https://github.com/kovidgoyal/kitty/releases/download/v0.45.0/kitty-0.45.0-x86_64.txz" -O kitty.txz
   
   rm -rf kitty
   mkdir -p kitty
-  tar -xf kitty.txz -C kitty
+  tar -xf kitty.txz -C kitty > /dev/null 2>&1
   rm kitty.txz
 
   ln -sf /opt/kitty/bin/kitty /usr/local/bin/kitty
@@ -243,33 +234,23 @@ function install_kitty() {
   echo -e "${greenColour}[+] Kitty instalada.${endColour}"
 }
 
-# --- 9. NEOVIM (ARREGLADO) ---
+# --- 9. NEOVIM ---
 function install_neovim() {
-  echo -e "\n${blueColour}[*] Instalando Neovim (Stable)...${endColour}\n"
+  echo -e "\n${blueColour}[*] Instalando Neovim (Stable)...${endColour}"
   cd /opt
-  wget "https://github.com/neovim/neovim/releases/download/v0.11.5/nvim-linux-x86_64.tar.gz" -O nvim.tar.gz
+  wget -q "https://github.com/neovim/neovim/releases/download/v0.11.5/nvim-linux-x86_64.tar.gz" -O nvim.tar.gz
 
-  # Borramos instalación previa
   rm -rf nvim
+  tar -xzf nvim.tar.gz > /dev/null 2>&1
   
-  # Descomprimimos
-  tar -xzf nvim.tar.gz
-  
-  # BUSQUEDA INTELIGENTE DE CARPETA
-  # Buscamos qué carpeta se acaba de crear (normalmente nvim-linux64, pero por si acaso)
   extracted_dir=$(find . -maxdepth 1 -type d -name "nvim-linux*" | head -n 1)
-
   if [ -n "$extracted_dir" ]; then
       mv "$extracted_dir" nvim
-      echo -e "   [i] Renombrado $extracted_dir -> nvim"
-  else
-      echo -e "${redColour}[!] Error: No encuentro la carpeta descomprimida de Neovim.${endColour}"
   fi
 
   rm nvim.tar.gz
-
   ln -sf /opt/nvim/bin/nvim /usr/local/bin/nvim
-  echo -e "${greenColour}[+] Neovim instalado en /opt/nvim.${endColour}"
+  echo -e "${greenColour}[+] Neovim instalado.${endColour}"
 }
 
 # --- 10. TOOLS EXTRA ---
@@ -278,10 +259,12 @@ function install_tools(){
   cd /usr/local/src
   
   lsd_url="https://github.com/lsd-rs/lsd/releases/download/v1.0.0/lsd_1.0.0_amd64.deb"
-  wget "$lsd_url" -O lsd.deb && dpkg -i lsd.deb && rm lsd.deb
+  wget -q "$lsd_url" -O lsd.deb && dpkg -i lsd.deb > /dev/null 2>&1 && rm lsd.deb
 
   bat_url="https://github.com/sharkdp/bat/releases/download/v0.24.0/bat_0.24.0_amd64.deb"
-  wget "$bat_url" -O bat.deb && dpkg -i bat.deb && rm bat.deb
+  wget -q "$bat_url" -O bat.deb && dpkg -i bat.deb > /dev/null 2>&1 && rm bat.deb
+  
+  echo -e "${greenColour}[+] Herramientas instaladas.${endColour}"
 }
 
 # --- 11. WALLPAPER ---
@@ -304,7 +287,7 @@ function install_wallpaper() {
 # --- EJECUCIÓN ---
 install_dependencies
 install_dotfiles
-install_zsh_omz # <--- NUEVA FUNCIÓN
+install_zsh_omz
 install_fonts
 install_bspwm_sxhkd
 install_polybar
